3b79e9c1774346b57fd4c4cbda73f097047e03d1
huy.tranduc <huy.tranduc@vti.com.vn>
2024-09-24 16:44:56 +0700

    cicd demo 9

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-linkkkkkkkkkkkkkkkk"
+          className="App-linkkkkkkkkkkkkk"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
